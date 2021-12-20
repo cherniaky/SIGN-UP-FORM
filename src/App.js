8008a373b22fb8d1268c9c811f01b99d8f4a5ac0
Yurii <juracherniak@gmail.com>
2021-12-20 21:49:25 +0100

    add basic form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import unlock from "./images/unlock.png";
 
 function App() {
     return (
@@ -77,6 +78,109 @@ function App() {
                     </ul>
                 </nav>
             </section>
+
+            <main>
+                <div className="sign-up-form-container">
+                    <section className="sign-up">
+                        <header>
+                            <h2 className="sign-up-header">
+                                Create an Intuit account
+                            </h2>
+                            <div>
+                                <p>
+                                    One account for everything intuit, including
+                                    Mint.
+                                </p>
+                                <a href="https://accounts-help.lc.intuit.com/questions/1582580-creating-an-account">
+                                    Learn More
+                                </a>
+                            </div>
+                        </header>
+
+                        <form className="sign-up-form">
+                            <fieldset>
+                                <label for="email">Email</label>
+                                <input
+                                    autoFocus
+                                    className="input email"
+                                    id="email"
+                                    required
+                                    maxLength="100"
+                                    type="email"
+                                    autoComplete="off"
+                                />
+                            </fieldset>
+                            <fieldset>
+                                <label for="phone-num">
+                                    Phone <small>(recommended)</small>
+                                </label>
+                                <input
+                                    autoFocus
+                                    className="input phone"
+                                    id="phone-num"
+                                    required
+                                    maxLength="30"
+                                    type="tel"
+                                    autoComplete="off"
+                                />
+                            </fieldset>
+                            <fieldset>
+                                <label for="password">Password</label>
+                                <div className="input-wrapper">
+                                    <input
+                                        autoFocus
+                                        className="input password"
+                                        id="password"
+                                        required
+                                        maxLength="100"
+                                        type="password"
+                                        autoComplete="off"
+                                    />
+                                    <img className="unlock" src={unlock} />
+                                </div>
+                            </fieldset>
+                            <fieldset>
+                                <label for="copassword">Confirm password</label>
+                                <div className="input-wrapper">
+                                    {" "}
+                                    <input
+                                        autoFocus
+                                        className="input copassword"
+                                        id="copassword"
+                                        required
+                                        maxLength="100"
+                                        type="password"
+                                        autoComplete="off"
+                                    />
+                                </div>
+                            </fieldset>
+                            <div>
+                                <button className="button-submit" type="submit">
+                                    {" "}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-6 w-6"
+                                        className="sign-in-button-icon"
+                                        fill="none"
+                                        width="20"
+                                        height="25"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                                        />
+                                    </svg>
+                                    Create Account
+                                </button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            </main>
         </div>
     );
 }
